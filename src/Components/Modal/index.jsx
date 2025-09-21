078d77e03c281b2ee73e91110d386f7012be5c46
Vale Machuca <valentinomachuca.dev@gmail.com>
2025-09-21 17:01:07 -0300

    Change text colors

--- src/Components/Modal/index.jsx
+++ src/Components/Modal/index.jsx
@@ -6,7 +6,7 @@ const Modal = ({onClose, item}) => {
   return (
     <motion.div className={s.container}>
       <motion.div className={s.content} layoutId={`modal_${item.id}`}>
-        <div onClick={onClose} className={s.close}>x</div>
+        <div onClick={onClose} className={s.close}>Click here to close, work in progress...</div>
       </motion.div>
     </motion.div>
   )
